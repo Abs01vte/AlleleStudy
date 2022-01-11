e530f628c036d1759723b3731d1ca8b72299a3f8
kyle <hammykyle@gmail.com>
2022-01-11 01:51:07 -0700

    reads files, makes a class of mates and prints

--- genetics.py
+++ genetics.py
@@ -4,3 +4,17 @@ class Mates:
     def __init__(self, atr1, atr2) :
         self.atr1 = atr1
         self.atr2 = atr2
+    def __repr__(self):
+        return self.atr1 + self.atr2
+    def __str__(self):
+        return "Mates"
+
+matelist = []
+
+with open('genes.txt') as f:
+    contents = f.readlines()
+    for line in contents :
+        atrs = line.split()
+        matelist.append(Mates(atrs[0], atrs[1]))
+        print(atrs)
+    print(matelist)
